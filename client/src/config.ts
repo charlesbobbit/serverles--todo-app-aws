eeabb7882c67d662544328c8859ff4889021f23d
Ifakachukwu Owoh <owohifeakachukwu@gmail.com>
2022-11-14 16:28:02 +0100

    initialize repository

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'vo0z3kaxxh';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-fjp-3wf9.us.auth0.com',
+  clientId: 'b4bG4TV8cDkqvafjImlMeEE4qUctL7Ti',
+  callbackUrl: 'http://localhost:3000/callback'
+};
